8aa4ff6378b5c49ac8c1c2e26f8dfc8949a6b309
jakupmichaelsen <jakupmichaelsen@gmail.com>
2019-08-13 10:20:05 +0200

    -

---
+++ 181213_STX_Engelsk_A_Vejledende_opgavesaet_1_2018_v2/js/video-player-subtitles.js
@@ -0,0 +1,103 @@
+var subtitles = {
+    T_181213_video_opg4b: {
+		lang: "en",
+		name: "English",
+        tracks: [
+			//from		To			Text
+			T(TTime("00:00:01:00"), TTime("00:00:05:00"), "ellentube"),
+			T(TTime("00:00:15:15"), TTime("00:00:23:00"), "MARCH FOR OUR LIVES"),
+			T(TTime("00:02:11:30"), TTime("00:02:21:10"), "Shutterfly $50,000"),
+			T(TTime("00:02:21:10"), TTime("00:02:23:30"), "MARCH FOR OUR LIVES"),
+			T(TTime("00:02:23:30"), TTime("00:02:27:25"), "MARCH 24 IN WASHINGTON, DC AND YOUR COMMUNITY<br />MARCHFOROURLIVES.COM"),
+			T(TTime("00:02:41:15"), TTime("00:02:47:00"), "ellentube.com")
+			
+        ]
+    }
+};
+
+/**
+ * Takes up to 4 values 
+ * 4 values: [0]hours[1]minutes[2]seconds[3]frames
+ * 3 values: [0]minutes[1]seconds[2]frames
+ * 2 values: [0]seconds[1]frames
+ * 1 value : [0]frames
+ */
+function ATime() {
+	var frames = 0;
+	var seconds = 0;
+	var minutes = 0;
+	var hours = 0;
+	if(arguments.length == 1) {
+		frames = (arguments[0]*1)/24;
+	}
+	else if(arguments.length == 2) {
+		frames = (arguments[1]*1)/24;
+		seconds = arguments[0]*1;
+	}
+	else if(arguments.length == 3) {
+		frames = (arguments[2]*1)/24;
+		seconds = arguments[1]*1;
+		minutes = (arguments[0]*1)*60;
+	}
+	else if(arguments.length == 4) {
+		frames = (arguments[3]*1)/24;
+		seconds = arguments[2]*1;
+		minutes = (arguments[1]*1)*60;
+		hours = (arguments[0]*1)*3600;
+	}
+	else {
+		return 0;
+	}
+	return hours + minutes + seconds + frames;
+}
+
+/**
+ * If value is undefined returns 0
+ * @param {*} value 
+ */
+function UndefinedToZero(value) {
+	if(typeof value === "undefined") {
+		return 0;
+	}
+	return value;
+}
+
+/**
+ * Convert input to seconds
+ * @param {numeric} frames Not Required
+ * @param {numeric} seconds Not Required
+ * @param {numeric} minutes Not Required
+ * @param {numeric} hours Not Required
+ */
+function _Time(frames, seconds, minutes, hours) {
+	frames = UndefinedToZero(frames) / 24;
+	seconds = UndefinedToZero(seconds);
+	minutes = UndefinedToZero(minutes) * 60;
+	hours = UndefinedToZero(hours) * 3600;
+	return hours + minutes + seconds + frames;
+}
+
+/**
+ * Convert string input "00:00:00:00" to seconds
+ * @param {string} value Required
+ */
+function TTime(value) {
+	if(typeof value === "undefined") {
+		value = "00:00:00:00";
+	}
+	var split = value.split(":");
+	var frames = (split[3]*1)/24;
+	var seconds = split[2]*1;
+	var minutes = (split[1]*1)*60;
+	var hours = (split[0]*1)*3600;
+	var output = hours + minutes + seconds + frames;
+	return output;
+}
+
+function T(start, end, text) {
+	return {
+		start: start,
+		end: end,
+		text: text
+	};
+}
