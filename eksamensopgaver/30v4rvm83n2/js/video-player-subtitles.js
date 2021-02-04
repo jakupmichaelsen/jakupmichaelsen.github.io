1a472e9a6a403903a2d5679f3ea3c8457b4ecb0a
jakupmichaelsen <jakupmichaelsen@gmail.com>
2021-02-04 10:29:35 +0100

    Error correction

---
+++ eksamensopgaver/30v4rvm83n2/js/video-player-subtitles.js
@@ -0,0 +1,116 @@
+var subtitles = {
+    T_181438_opg2: {
+		lang: "en",
+		name: "English",
+        tracks: [
+			//from		To			Text
+			T(TTime("00:00:00:01"), TTime("00:00:15:00"), "Word of the Year")
+        ]
+    },
+	
+    T_181438_opg5b: {
+		lang: "en",
+		name: "English",
+        tracks: [
+			//from		To			Text
+			T(TTime("00:00:00:01"), TTime("00:00:03:00"), "Kun tekst, der er relevant for besvarelsen, er tekstet til talesyntese."),
+			T(TTime("00:00:03:00"), TTime("00:00:07:00"), "Brooklyn College"),
+			T(TTime("00:00:08:15"), TTime("00:00:14:15"), "Brooklyn College<br />The City University of New York"),
+			T(TTime("00:00:24:15"), TTime("00:00:30:25"), "Bernie Sanders<br />US Senator, State of Vermont"),
+			T(TTime("00:00:30:25"), TTime("00:00:37:00"), "Bernie Sanders<br />Keynote Speaker"),
+			T(TTime("00:00:37:00"), TTime("00:00:50:00"), "Bernie Sanders<br />Honorary Degree, Doctor of Humane Letters")
+			
+        ]
+    }
+};
+
+/**
+ * Takes up to 4 values 
+ * 4 values: [0]hours[1]minutes[2]seconds[3]frames
+ * 3 values: [0]minutes[1]seconds[2]frames
+ * 2 values: [0]seconds[1]frames
+ * 1 value : [0]frames
+ */
+function ATime() {
+	var frames = 0;
+	var seconds = 0;
+	var minutes = 0;
+	var hours = 0;
+	if(arguments.length == 1) {
+		frames = (arguments[0]*1)/24;
+	}
+	else if(arguments.length == 2) {
+		frames = (arguments[1]*1)/24;
+		seconds = arguments[0]*1;
+	}
+	else if(arguments.length == 3) {
+		frames = (arguments[2]*1)/24;
+		seconds = arguments[1]*1;
+		minutes = (arguments[0]*1)*60;
+	}
+	else if(arguments.length == 4) {
+		frames = (arguments[3]*1)/24;
+		seconds = arguments[2]*1;
+		minutes = (arguments[1]*1)*60;
+		hours = (arguments[0]*1)*3600;
+	}
+	else {
+		return 0;
+	}
+	return hours + minutes + seconds + frames;
+}
+
+/**
+ * If value is undefined returns 0
+ * @param {*} value 
+ */
+function UndefinedToZero(value) {
+	if(typeof value === "undefined") {
+		return 0;
+	}
+	return value;
+}
+
+/**
+ * Convert input to seconds
+ * @param {numeric} frames Not Required
+ * @param {numeric} seconds Not Required
+ * @param {numeric} minutes Not Required
+ * @param {numeric} hours Not Required
+ */
+function _Time(frames, seconds, minutes, hours) {
+	frames = UndefinedToZero(frames) / 24;
+	seconds = UndefinedToZero(seconds);
+	minutes = UndefinedToZero(minutes) * 60;
+	hours = UndefinedToZero(hours) * 3600;
+	return hours + minutes + seconds + frames;
+}
+
+/**
+ * Convert string input "00:00:00:00" to seconds
+ * @param {string} value Required
+ */
+function TTime(value) {
+	if(typeof value === "undefined") {
+		value = "00:00:00:00";
+	}
+	var split = value.split(":");
+	var frames = (split[3]*1)/24;
+	var seconds = split[2]*1;
+	var minutes = (split[1]*1)*60;
+	var hours = (split[0]*1)*3600;
+	var output = hours + minutes + seconds + frames;
+	return output;
+}
+
+function T(start, end, text, direction) {
+	if(typeof direction === "undefined") {
+		direction = "ltr";
+	}
+	return {
+		start: start,
+		end: end,
+		text: text,
+		direction: direction
+	};
+}
